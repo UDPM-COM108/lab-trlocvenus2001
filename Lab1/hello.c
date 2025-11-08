0628951ba1fabef1a3c6c5296b5c84f10d995b30
Huỳnh Trường Lộc <trloc.venus2001@gmail.com>
2025-11-08 14:00:07 +0700

    nộp bài lab 2

--- Lab1/hello.c
+++ Lab1/hello.c
@@ -1,14 +1,7 @@
 #include <stdio.h> // thư viện
 
 int main(){ // hàm chính
-  //printf("Hello World!"); // Xuất ra màn hình Hello World!
-  //printf("\nHello World!"); \n là xuống dòng
-  int a,b; // khai báo biến a,b kiểu số nguyên
-  char ten[10]; // khai báo biến ten kiểu chuỗi ký tự có độ dài 10
-  printf("Nhap a và b: "); // xuất ra màn hình
-  scanf("%f %f", &a, &b); // nhập vào a và b
-  printf("Nhap vao tên tôi: "); // xuất ra màn hình
-  scanf(" %s", &ten); // nhập vào tên tôi
-  printf("\nXin chào tôi tên là: %s", ten); // xuất ra màn hình
+  printf("Hello\nWorld!"); // Xuất ra màn hình
+  printf("\nChao mung ban den voi lap trinh C!"); // Xuất ra màn hình
   return 0; // kết thúc hàm chính
 } // kết thúc hàm chính
---
+++ Lab2/lab2.c
@@ -0,0 +1,39 @@
+//Bài 1: XÂY DỰNG CHƯƠNG TRÌNH +- 2 SỐ NGUYÊN
+/*include <stdio.h>
+int main (){
+  int a,b,tong,hieu; // khai báo biến a,b kiểu số nguyên
+    printf("Nhap a và b: "); // xuất ra màn hình
+    scanf("%d %d", &a, &b); // nhập vào a và b
+    tong = a + b; // tính tổng
+    hieu = a - b; // tính hiệu
+    printf("\nTổng của a và b là: %d", tong); // xuất ra
+    printf("\nHiệu của a và b là: %d", hieu); // xuất ra
+    return 0;
+}*/
+
+//Bài 2: XÂY DỰNG CHƯƠNG TRÌNH TÍNH CHU VI VÀ DIỆN TÍCH HÌNH CHỮ NHẬT
+/*#include <stdio.h> // thư viện
+int main(){ // hàm chính
+  int a,b,P,S; // khai báo biến a,b kiểu số nguyên
+    printf("Nhap a và b: "); // xuất ra màn hình
+    scanf("%d %d", &a, &b); // nhập vào a và b 
+    P = (a + b) * 2; // tính chu vi
+    S = a * b; // tính diện tích
+    printf("Chu vi hinh chu nhat la: %d", P); // xuất ra màn hình
+    printf("\nDien tich hinh chu nhat la: %d", S); // xuất ra màn hình
+    return 0; // kết thúc hàm chính
+}*/
+
+//BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH TÍNH CHU VI VÀ DIỆN TÍCH HÌNH TRÒN
+/*#define PI 3.14
+#include <stdio.h> // thư viện
+int main(){ // hàm chính
+  float r,P,S; // khai báo biến r kiểu số thực
+    printf("Nhap ban kinh hinh tron: "); // xuất ra màn hình
+    scanf("%f", &r); // nhập vào r
+    P = 2 * PI * r; // tính chu vi
+    S = PI * r * r; // tính diện tích
+    printf("Chu vi hinh tron la: %.2f", P); // xuất ra màn hình
+    printf("\nDien tich hinh tron la: %.2f", S); // xuất ra màn hình
+    return 0; // kết thúc hàm chính
+}*/
