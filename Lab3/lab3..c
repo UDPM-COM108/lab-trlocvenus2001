f1e99dca6085ce1c5bcb2b325b39b72c1bc7b6ef
Huỳnh Trường Lộc <trloc.venus2001@gmail.com>
2025-11-14 18:42:31 +0700

    Update lab 3

---
+++ Lab3/lab3..c
@@ -0,0 +1,136 @@
+/*// Bài 1: Viết chương trình xây dựng tính học lực
+#include <stdio.h> // thư viện
+
+int main (){ 
+    double dtb;
+    printf("Nhập điểm trung bình từ 0 đến 10:"); // nhập điểm của sinh viên
+    scanf("%lf",&dtb);
+    // Xử lí logic (Thuật toán)
+    if (dtb < 0 || dtb > 10){ // Nếu nhập số không thuộc [0,10] thì xuất báo lỗi
+        printf("Lỗi! Điểm trung bình phải từ 0 đến 10.\n");
+    }
+    // Xét học lực dựa trên điểm trung bình
+    else if (dtb>=9){ // Nếu kết quả lớn hơn bằng 9 -> Bạn thuộc top học lực xuất sắc
+        printf("Học lực xuất sắc\n");
+    }
+    else if (dtb>=8){ // Nếu kết quả lớn hơn bằng 8 -> Bạn thuộc top học lực giỏi
+        printf("Học lực giỏi\n");
+    }
+    else if (dtb>=6.5){ // Nếu kết quả lớn hơn bằng 6.5 -> Bạn thuộc top học lực khá
+        printf("Học lực khá\n");
+    }
+    else if (dtb>=5){ // Nếu kết quả lớn hơn bằng 5 -> Bạn thuộc top học lực trung bình
+        printf("Học lực trung bình");
+    }
+    else if (dtb>=3.5){ // Nếu kết quả lớn hơn bằng 3.5 -> Bạn thuộc top học lực yếu
+        printf("Học lực yếu");
+    }
+    else{ // Nếu kết quả nhỏ hơn 3.5 -> Bạn thuộc top học lực kém
+        printf("Học lực kém");
+    }
+    return 0; // kết thúc
+}*/
+
+/*// Bài 2: Viết chương trình xây dựng phương trình bậc 1
+#include <stdio.h>
+
+int main() {
+    double a, b, x;  // Khai báo biến a, b và nghiệm x
+    printf("Phương trình có dạng ax + b = 0\n");
+    printf("-------------------------------\n");
+    // Nhập dữ liệu
+    printf("Nhập giá trị a và b: ");
+    scanf("%lf %lf", &a, &b);
+    // Xử lý logic (Thuật toán)
+    if (a == 0) { // TH: a = 0 -> Ta có 2 trường hợp như sau:
+        if (b == 0) {
+            // TH: b = 0 -> 0x + 0 = 0 -> Luôn đúng 
+            printf("Phương trình vô số nghiệm.\n");
+        } else { 
+            // TH: b khác 0 (b != 0) -> 0x + b = 0 -> Vô lý 
+            printf("Phương trình vô nghiệm.\n");
+        }
+    } else {
+        // TH a khác 0 (a != 0)-> Có 1 nghiệm duy nhất x = -b/a
+        x = -b / a;
+        printf("Phương trình có nghiệm x = %lf\n", x);
+    }
+    return 0; // Kết thúc
+}*/
+/*// Bài 3: Viết chương trình xây dựng phương trình bậc 2
+#include <stdio.h>
+#include <math.h> // Dùng thư viện này để giải phương trình bậc 2 
+
+int main() {
+    double a, b, c, x1, x2, delta; // Khai báo biến a, b, c, các nghiệm x và delta
+    printf("Phương trinh có dạng ax2 + bx + c = 0\n");
+    printf("-------------------------------------\n");
+    // Nhập dữ liệu
+    printf("Nhập giá trị a, b và c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    // Xử lí logic (Thuật toán)
+    if (a == 0) { // TH: a = 0 -> Phương trình bậc nhất
+        printf("Phương trình bậc nhất.\n");
+        printf("----------------------\n");
+        if (b == 0) { // TH b = 0 -> Ta có 2 trường hợp như sau:
+            if (c == 0) {
+                // TH: c = 0 -> 0x^2 + 0x + 0 = 0 -> Luôn đúng
+                printf("Phương trình vô số nghiệm.\n");
+            } else { 
+                // TH: c khác 0 (c != 0)-> 0x^2 + 0x + c = 0 -> Vô lý
+                printf("Phương trình vô nghiệm.\n");
+            }
+        } else {
+            x1 = -c / b;
+            printf("Phương trình có nghiệm x = %.lf\n, x1");
+        }
+    }
+    else { // TH: a khác 0 (a! = 0) -> Phương trình bậc 2
+        // Công thức delta
+        delta = (b*b - 4*a*c);
+        if (delta < 0) { // TH: Delta < 0 -> Phương trình vô nghiệm
+            printf("Delta = %.lf < 0\n", delta);
+            printf("Phương trình vô nghiệm.\n");
+        }
+        else if (delta == 0 ){ // TH: Delta = 0 -> Phương trình có nghiệm kép 
+            x1 = -b / (2*a);
+            printf("Delta = 0\n");
+            printf("Phương trình có nghiệm kép x1 = x2 = %.lf\n", x1);
+        }
+        else { // TH: Delta > 0 -> Phương trình có 2 nghiệm riêng biệt
+            x1 = (-b + sqrt(delta))/(2*a);
+            x2 = (-b + sqrt(delta))/(2*a);
+            printf("Delta = %.lf > 0\n", delta);
+            printf("Phương trình có 2 nghiệm riêng biệt.\n");
+            printf("x1 = %.lf\n", x1);
+            printf("x2 = %.lf\n", x2);
+        }
+    }
+    return 0;
+}*/
+
+/*// Bài 4: Sử dụng switch-case để thực hiện tạo menu cho 3 bài tập trên
+#include <stdio.h>
+
+int main(){
+    int luachon;
+    printf("Chọn chương trình tính toán:\n");
+    scanf("%d",&luachon);
+    switch (luachon){
+        case 1:
+            printf("Tính học lực:");
+        break;
+        case 2:
+            printf("Tính phương trình bậc nhất: ");
+        break;
+        case 3:
+            printf("Tính phương trình bậc hai:");
+        break;
+        return 0;
+        default:
+            printf("Lỗi: Lựa chọn không hợp lệ, vui lòng chọn lại từ 0 đến 3.\n");
+        break;
+        }
+        printf("\n");
+    return 0;
+}*/
