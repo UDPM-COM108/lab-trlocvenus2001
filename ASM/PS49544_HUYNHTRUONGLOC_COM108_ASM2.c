985898d7e5c5ac8cdf8284b626559b0da08e5ceb
Huỳnh Trường Lộc <trloc.venus2001@gmail.com>
2025-12-04 06:09:52 +0700

    update asm2 lan 1

---
+++ ASM/PS49544_HUYNHTRUONGLOC_COM108_ASM2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(){
+    int chon;
+    do {
+        printf("");
+        //...
+        printf("");
+        scanf("%d", &chon);
+        switch(chon){
+            case 1: {
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+                break;
+            }
+            case 6: {
+                break;
+            }
+            case 7: {
+                break;
+            }
+            case 8: {
+                break;
+            }
+            case 9: {
+                break;
+            }
+            case 10: {
+                break;
+            }
+            case 0: {
+                break;
+            }
+            default:
+                printf("");
+        }
+    } while (chon != 0);  
+}
